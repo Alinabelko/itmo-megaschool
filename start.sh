84767034eb63a3bf6837013d1b47c1be0150850c
Alina Belko <alinabelko@gmail.com>
2025-01-31 17:17:06 +0100

    Добавлены настройки для отсутсвия таймаута при большом числе запросов

--- start.sh
+++ start.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-gunicorn main:app --workers 6 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:80
+gunicorn main:app \
+    --workers 6 \
+    --worker-class uvicorn.workers.UvicornWorker \
+    --bind 0.0.0.0:80 \
+    --backlog 2048 \
+    --worker-connections 1000 \
+    --keep-alive 5 \
+    --timeout 300
